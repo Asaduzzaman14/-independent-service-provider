751b7c72c7363055d406ba2cc8bc64436dac4bb3
Md Asaduzaman <asadasad9282@gmail.com>
2022-04-18 19:12:53 +0600

    about page complete

--- src/Component/Pages/About/About.js
+++ src/Component/Pages/About/About.js
@@ -3,7 +3,8 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h3>This is about page</h3>
+            <h3>Name:Md Asaduzzaman</h3>
+            <p>My goal to be became a successful full stact web developer. To achive my goal I am trying to do my best practice and best focus on the work. Continuously i am doing hard work and facing new new Challenge. I am always trying to my level best.</p>
         </div>
     );
 };
